debbc9c2307e501803b3ca79485a5db2e67241ac
Anand Tyagi <ananddtyagi@yahoo.com>
2019-12-20 14:46:17 -0500

    learned how to use components in React

---
+++ react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Introduction from './components/introduction'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Introduction></Introduction>
+      </header>
+    </div>
+  );
+}
+
+export default App;
