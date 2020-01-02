e98d36c2013a6602ae8fee1f4d38623bf7a484b1
Anand Tyagi <ananddtyagi@yahoo.com>
2020-01-02 17:42:56 -0500

    fixed formatting issues

--- react/src/components/aboutme.jsx
+++ react/src/components/aboutme.jsx
@@ -9,14 +9,14 @@ export default class AboutMe extends Component {
     return (
       <div>
         <h1 className="name">ANAND TYAGI</h1>
-        <p> Hi! I'm Anand. I'm a Junior at NYU studying Computer Science and Math. I have a passion for CS and Entrepreneurship and enjoy working on tough and interesting problems. </p>
+        <p className="bio"> Hi! I'm Anand. I'm a Junior at NYU studying Computer Science and Math. I have a passion for CS and Entrepreneurship and enjoy working on tough and interesting problems. </p>
         <a target="_blank" href="https://github.com/ananddtyagi">
           <img className="icon" src={github}/>
         </a>
         <a target="_blank" href="https://www.linkedin.com/in/ananddtyagi/">
           <img className="icon" src={linkedin}/>
         </a>
-        
+
       </div>
     )
   }
