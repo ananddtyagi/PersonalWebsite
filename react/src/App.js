b5a5d9bb6ff2277e638385a7424c41afa403e810
Anand Tyagi <ananddtyagi@yahoo.com>
2020-01-05 23:14:59 -0500

    made the sidebar collapsible

--- react/src/App.js
+++ react/src/App.js
@@ -5,15 +5,58 @@ import './App.css';
 import Drawer from '@material-ui/core/Drawer';
 import Hidden from '@material-ui/core/Hidden';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import Sidebar from './components/sidebar';
 import Main from './components/main';
 
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    height: '100vh',
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+
+  },
+}));
+
+
 function App(props) {
   const { container } = props;
-
+  const classes = useStyles();
+  const theme = useTheme();
   const [mobileOpen, setMobileOpen] = React.useState(false);
+
   const handleDrawerToggle = () => {
       setMobileOpen(!mobileOpen);
     };
@@ -21,8 +64,18 @@ function App(props) {
 
   return (
     <div className="App">
-      <div className="Sidebar">
-      <nav>
+
+      <div >
+      <nav className={classes.drawer}>
+      <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+        </IconButton>
         <Hidden smUp implementation="css">
           <Drawer
              anchor={'left'}
@@ -35,8 +88,9 @@ function App(props) {
              }}
           >
           {(
-
+            <div className="Sidebar">
               <Sidebar></Sidebar>
+            </div>
 
           )}
           </Drawer>
@@ -56,11 +110,14 @@ function App(props) {
          </Hidden>
       </nav>
       </div>
-      <main>
+        <div className = {classes.content}>
+        <main>
         <div className = "Main">
-          <Main></Main>
-        </div>
-      </main>
+
+            <Main></Main>
+          </div>
+        </main>
+      </div>
 
     </div>
   );
