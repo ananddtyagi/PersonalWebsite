8dfe5c90c80121de960716dfc92610e1c358bc0d
Anand Tyagi <ananddtyagi@yahoo.com>
2020-01-05 22:15:23 -0500

    using more material ui components

--- react/src/App.js
+++ react/src/App.js
@@ -1,22 +1,68 @@
 import React from 'react';
-
 import logo from './logo.svg';
 import './App.css';
+
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
+
+
 import Sidebar from './components/sidebar';
 import Main from './components/main';
 
-function App() {
+function App(props) {
+  const { container } = props;
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+      setMobileOpen(!mobileOpen);
+    };
+
 
   return (
-      <div className="App">
-        <div className="Sidebar">
-          <Sidebar></Sidebar>
-        </div>
+    <div className="App">
+      <div className="Sidebar">
+      <nav>
+        <Hidden smUp implementation="css">
+          <Drawer
+             anchor={'left'}
+             container={container}
+             variant="temporary"
+             open={mobileOpen}
+             onClose={handleDrawerToggle}
+             ModalProps={{
+               keepMounted: true, // Better open performance on mobile.
+             }}
+          >
+          {(
+
+              <Sidebar></Sidebar>
+
+          )}
+          </Drawer>
+        </Hidden>
+
+        <Hidden xsDown implementation="css">
+           <Drawer
+             variant="permanent"
+             open
+           >
+           {(
+             <div className="Sidebar">
+               <Sidebar></Sidebar>
+             </div>
+           )}
+           </Drawer>
+         </Hidden>
+      </nav>
+      </div>
+      <main>
         <div className = "Main">
           <Main></Main>
         </div>
+      </main>
 
-      </div>
+    </div>
   );
 }
 
