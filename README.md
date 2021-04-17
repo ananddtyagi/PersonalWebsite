3136c819d609407a578491095c9e735397e6a400
anandtyagiklarity <anand.tyagi@klaritylaw.com>
2021-04-17 12:50:25 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-Hello! This is the repository for my personal website which you can see at https://anandtyagi.me
-
-If you see any problems with the website, please feel free to create an issue on this repository to let me know.
---
+++ precache-manifest.b142ec5af0e0452c4412c495d00b07be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989c0b0c7164cf903cb95db026c5a9c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36315fb5f1f899465187",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21380057521f96d39086",
+    "url": "/static/js/2.6cbd5eb6.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.6cbd5eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36315fb5f1f899465187",
+    "url": "/static/js/main.524a86f7.chunk.js"
+  },
+  {
+    "revision": "cb5e4625273608814864",
+    "url": "/static/js/runtime-main.4d26892b.js"
+  },
+  {
+    "revision": "feb6c02597640cb2939bc0d3b1c11445",
+    "url": "/static/media/prof_pic.feb6c025.png"
+  }
+]);
